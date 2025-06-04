b78fa60137e927e9db0af610d3e2b3bd15177df9
marily77 <93117408+marily77@users.noreply.github.com>
2025-06-04 19:07:29 +0300

    Updates

---
+++ precache-manifest.8f1b7725fad8a74b93be3acd49d182fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4d3fbf2387ca3087aded99553f88ba",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5869769774dc92125785",
+    "url": "/robofriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "23f13d2237049e802051",
+    "url": "/robofriends/static/css/main.57d81a79.chunk.css"
+  },
+  {
+    "revision": "5869769774dc92125785",
+    "url": "/robofriends/static/js/2.04da0826.chunk.js"
+  },
+  {
+    "revision": "23f13d2237049e802051",
+    "url": "/robofriends/static/js/main.62af3ff5.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
